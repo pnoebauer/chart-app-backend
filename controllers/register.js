c0d86259703d25bcc0000c67fab82d9caf3fe0d3
Philip Noebauer <pnoebauer>
2020-05-21 16:45:39 +0800

    included transaction

--- controllers/register.js
+++ controllers/register.js
@@ -1,18 +1,29 @@
 const handleRegister = (req,res,db) => {
-	const { email, name } = req.body;
+	const { email, name, password } = req.body;
 	
-	db
-		.insert({
-			email: email,
-			name: name,
-			joined: new Date()
+	db.transaction(trx => {
+		trx.insert({
+			hash: password,
+			email: email
 		})
-		.into('users')
-		.returning('*')
-		//.then(function(user){console.log(user)});
-		.then(user => res.json(user[0]))	
-		// .catch(err => res.status(400).json(err));
-		.catch(err => res.status(400).json('unable to register'));
+		.into('login')
+		.returning('email')
+		.then(loginEmail => res.json(loginEmail[0])
+			// trx('users')
+			// .insert({
+			// 	email: loginEmail[0],
+			// 	name: name,
+			// 	joined: new Date()
+			// })
+			// .returning('*')
+			// .then(user => res.json(user[0]))	
+			// // .catch(err => res.status(400).json('unable to register'));
+		)
+	.then(trx.commit)
+	.catch(trx.rollback)
+	})
+	.catch(err => res.status(400).json('unable to register'));
+
 }
 
 module.exports = {
