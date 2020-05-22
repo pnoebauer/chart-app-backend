a245229e5cf53453a7c5fa11db9aed3ea174a109
Philip Noebauer <pnoebauer>
2020-05-22 18:17:20 +0800

    check email and password

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,34 +1,29 @@
 const handleSignin = (req,res,db) => {
 	const {email, password} = req.body;
 
-	// db('users').where({
-	//   email: email,
-	// }).select('*')
-	// .then(
-	// 	user => {
-	// 		if(user.length > 0){
-	// 			return res.json(user[0]);
-	// 		}
-	// 		else {
-	// 			return res.json('user does not exist');
-	// 		}
-	// 	} 
-	// )
-	// .catch(err => res.json('incorrect input variables'));
-
-	db('users').where({email: email})
-	.then(data => db('users').select('*')
-					.where('email','=',data[0].email)
-	.then(
-		user => {
-			if(user.length > 0){
-				return res.json(user[0]);
+	db('login').where({email: email})
+	.then(userInfo => 
+		{
+			if(userInfo[0].hash === password) 
+			{
+				return db('users').select('*')
+					.where('email','=',userInfo[0].email)
+					.then(
+						user => {
+							if(user.length > 0){
+								return res.json(user[0]);
+							}
+							else {
+								return res.json('user does not exist');
+							}
+						} 
+					)
 			}
 			else {
-				return res.json('user does not exist');
+				return res.json('the password/email combination is incorrect');
 			}
-		} 
-	))
+		}
+	)
 	.catch(err => res.json('incorrect input variables'));
 }
 
