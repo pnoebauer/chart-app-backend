d540b97350dbf793a994e50c9c208826c37bec9c
Philip Noebauer <pnoebauer>
2020-06-08 09:24:12 +0800

    adjusting data conversion for front end

--- controllers/data.js
+++ controllers/data.js
@@ -4,14 +4,15 @@ const stringify = require('csv-stringify');
 const posts = require('../posts.json');
 const d3 = require("d3-time-format");
 
-const convertJSON = (data) => {
+const convertCSV = (data) => {
   return (
     stringify(data, { 
       header: true,
       cast: {
         date: value => {
           // console.log(value);
-            const formatTime = d3.timeFormat("%Y-%m-%d %H:%M")
+            // const formatTime = d3.timeFormat("%Y-%m-%d %H:%M")
+            const formatTime = d3.timeFormat("%Y-%m-%d")
             return formatTime(value);
         }
       }
@@ -28,25 +29,25 @@ const dbToFrontend = (req,res,db) => {
   res.setHeader('Pragma', 'no-cache');
 
   // db('sp500').where('id','<',10).then(data=>res.send(data));
-  // db('sp500').where('id','<',10000).then(data => convertJSON(data).pipe(res));
+  // db('sp500').where('id','<',10000).then(data => convertCSV(data).pipe(res));
 
-const period = 'hour';
+const period = 'day';
 
+// COUNT(*) ticks, \
 db('sp500')
-  .select(db.raw(`date_trunc('${period}', datetime) datetime, \
-    (array_agg(openprice ORDER BY datetime ASC))[1] o, \
-    MAX(highprice) h, \
-    MIN(lowprice) l, \
-    (array_agg(closeprice ORDER BY datetime DESC))[1] c, \
-    SUM(volume) volume, \
-    COUNT(*) ticks`))
-  .whereBetween('datetime', ['2000-11-08', '2000-12-08'])
+  .select(db.raw(`date_trunc('${period}', datetime) "date", \
+    (array_agg(openprice ORDER BY datetime ASC))[1] "open", \
+    MAX(highprice) "high", \
+    MIN(lowprice) "low", \
+    (array_agg(closeprice ORDER BY datetime DESC))[1] "close", \
+    SUM(volume) volume`))
+  // .whereBetween('datetime', ['2000-11-08', '2000-12-08'])
   .groupBy(db.raw(`date_trunc('${period}', datetime)`))
-  .orderBy('datetime')
-  .limit(50)
+  .orderBy('date')
+  // .limit(50)
   // .then(data=>console.log(data))
   // .then(data=>res.send(data));
-  .then(data => convertJSON(data).pipe(res)
+  .then(data => convertCSV(data).pipe(res)
   );
 }
 
